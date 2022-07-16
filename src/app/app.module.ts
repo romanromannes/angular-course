d52c3077d5bb0fec1c461c0fb540e3b3a5cbc5bc
romanromannes <romanromannes@gmail.com>
2022-07-15 22:43:28 -0300

    add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+import { LeftNavComponent } from './components/left-nav/left-nav.component';
+import { ContentAreaComponent } from './components/content-area/content-area.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    LeftNavComponent,
+    ContentAreaComponent
   ],
   imports: [
     BrowserModule
